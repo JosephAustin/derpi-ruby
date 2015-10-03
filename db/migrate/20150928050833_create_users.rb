f026b47011979aee888f6b95cfd4166abdc7e62d
Joseph Austin <joseph.the.austin@gmail.com>
2015-10-03 18:19:11 -0500

    Whoops...

--- db/migrate/20150928050833_create_users.rb
+++ db/migrate/20150928050833_create_users.rb
@@ -2,11 +2,11 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :key
-      t.text :best_tags, :text
-      t.text :bad_tags, :text
-      t.text :good_tags, :text
-      t.text :worst_tags, :text
-      t.text :file_tags, :text
+      t.text :best_tags
+      t.text :bad_tags
+      t.text :good_tags
+      t.text :worst_tags
+      t.text :file_tags
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20150928060230) do
   create_table "users", force: :cascade do |t|
     t.string "key"
     t.text   "best_tags"
-    t.text   "text"
     t.text   "bad_tags"
     t.text   "good_tags"
     t.text   "worst_tags"
